9c561e1c2069bc9fe87dbe92ab53cd55e4faedea
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-03-11 18:04:51 +0700

    config tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,9 +18,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className='btn-primary text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800' onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button>Masuk</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
