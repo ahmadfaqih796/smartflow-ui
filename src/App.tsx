48d94ccf950ab3fb7df0fc0ed36621323dbc57f6
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-03-11 18:08:35 +0700

    fix arsitektur tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-// import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,7 +20,7 @@ function App() {
         <button className='btn-primary text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800' onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <button>Masuk</button>
+        <button className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>Masuk</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
