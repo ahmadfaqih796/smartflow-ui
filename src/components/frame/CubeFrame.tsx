c37ea45dd28e26360832ada06897c7fb3e04a8ba
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-04-17 10:13:18 +0700

    style background cube frame

---
+++ src/components/frame/CubeFrame.tsx
@@ -0,0 +1,43 @@
+import "@/assets/styles/tailwindcss/animations/frames/cube.css";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+  color? : "red" | "blue";
+};
+
+const CubeFrame: React.FC<Props> = ({ children, color }) => {
+   const getColor = (value?: string) => {
+     switch (value) {
+       case "red":
+         return "bg-red-400 dark:bg-red-800";
+       case "blue":
+         return "bg-blue-400 dark:bg-blue-800";
+       default:
+         return "bg-gray-400 dark:bg-gray-500";
+     }
+   }
+
+   const getBackgroundColor = (value?: string) => {
+     switch (value) {
+       case "red":
+         return "bg-red-200 dark:bg-red-950";
+       case "blue":
+         return "bg-blue-200 dark:bg-blue-950";
+       default:
+         return "bg-gray-200 dark:bg-gray-800";
+     }
+   }
+  return (
+    <main className={`area w-full h-screen flex items-center justify-center ${getBackgroundColor(color)}`}>
+      <ul className="cubes">
+        {Array.from({ length: 10 }).map((_, i) => (
+          <li key={i} className={`${getColor(color)}`} />
+        ))}
+      </ul>
+      <div className="relative z-10">{children}</div>
+    </main>
+  );
+};
+
+export default CubeFrame;
--- src/pages/errors/404.page.tsx
+++ src/pages/errors/404.page.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import CubeFrame from "@/components/frame/CubeFrame";
+import React from "react";
 
-const NotFoundPage : React.FC = () => {
+const NotFoundPage: React.FC = () => {
   return (
-    <div>NotFoundPage</div>
-  )
-}
+    <CubeFrame>
+      <div className="text-center">
+        <p className="text-base font-semibold text-indigo-600 dark:text-indigo-200">404</p>
+        <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+          Page Not Found
+          {/* Halaman Tidak Ditemukan */}
+        </h1>
+        <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8 dark:text-gray-200">
+          Sorry, we couldn’t find the page you’re looking for.
+          {/* Maaf, kami tidak dapat menemukan halaman yang Anda cari. */}
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <button
+            onClick={() => window.history.back()}
+            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Go back home
+          </button>
+          <a href="#" className="text-sm font-semibold text-gray-900 dark:text-gray-100">
+            Contact Faqih <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
+      </div>
+    </CubeFrame>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
