bf022df83cb3e676bede0f382b3fd6f4231e39d8
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-04-21 23:51:49 +0700

    fix start node

--- src/components/flow/elements/StartNode.tsx
+++ src/components/flow/elements/StartNode.tsx
@@ -2,8 +2,16 @@ import { Handle, NodeProps, Position, useStore } from "reactflow";
 import { COLOR_SHAPE_FLOW_DIAGRAM } from "../constants/digram.constant";
 
 const StartNode = ({ id, data }: NodeProps) => {
+  const nodes = useStore((state) => state.nodeInternals);
   const edges = useStore((state) => state.edges);
+  const nodesArray = Array.from(nodes.values());
+  const startNodes = nodesArray.filter((node) => node.type === "start");
+  console.log("eddd", startNodes);
   const outgoing = edges.filter((edge) => edge.source === id);
+  // if (startNodes.length >= 1) {
+  //   alert("Hanya boleh 1 Start Node!");
+  //   return;
+  // }
   return (
     <div
       className={`w-[140px] h-[88px] ${COLOR_SHAPE_FLOW_DIAGRAM.start} relative`}
@@ -20,6 +28,7 @@ const StartNode = ({ id, data }: NodeProps) => {
           // transform: "none"
         }}
         isValidConnection={() => outgoing.length === 0}
+      
       />
     </div>
   );
