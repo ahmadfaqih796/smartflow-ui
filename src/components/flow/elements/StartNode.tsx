3cad8bdd9cb76998bbf6406b0f20bb2021f6f3ce
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-04-23 16:49:21 +0700

    handle start node only 1

--- src/components/flow/elements/StartNode.tsx
+++ src/components/flow/elements/StartNode.tsx
@@ -1,9 +1,26 @@
-import { Handle, NodeProps, Position, useStore } from "reactflow";
+import { useAlert } from "@/context/AlertContext";
+import React from "react";
+import { Handle, NodeProps, Position, useReactFlow, useStore } from "reactflow";
 import { COLOR_SHAPE_FLOW_DIAGRAM } from "../constants/digram.constant";
 
 const StartNode = ({ id }: NodeProps) => {
+  const { showAlert } = useAlert();
+  const { setNodes } = useReactFlow();
+
+  const nodes = useStore((state) => state.nodeInternals);
   const edges = useStore((state) => state.edges);
+
   const outgoing = edges.filter((edge) => edge.source === id);
+  const totalStartNodes = React.useMemo(() => {
+    return Array.from(nodes.values()).filter((node) => node.type === "start")
+      .length;
+  }, [nodes]);
+  React.useEffect(() => {
+    if (totalStartNodes > 1) {
+      setNodes((prevNodes) => prevNodes.filter((node) => node.id !== id));
+      showAlert("Maaf, 'Start' tidak boleh lebih dari satu!", "error");
+    }
+  }, [totalStartNodes]);
   return (
     <div
       className={`w-[180px] h-[88px] ${COLOR_SHAPE_FLOW_DIAGRAM.start} relative`}
